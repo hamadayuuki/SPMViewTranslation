82e1a699c5ac1b04785f80b262459dffe2ad0a01
hamadayuuki <yuukirinnma@gmail.com>
2023-12-01 11:32:46 +0900

    Init project

---
+++ SPMViewTranslation/SPMViewTranslation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SPMViewTranslation
+//
+//  Created by 濵田　悠樹 on 2023/12/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
